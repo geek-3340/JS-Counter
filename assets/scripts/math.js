448e178d612a635593584c39044bbaed6748a9a5
GEEK <crew.sento0864@gmail.com>
2025-03-07 00:34:18 +0900

    math.js を更新

--- assets/scripts/math.js
+++ assets/scripts/math.js
@@ -1,12 +1,45 @@
 (() => {
   const $counter = document.getElementById('js-counter');
+  const $logo = document.getElementById('js-logo');
+  
+  let currentMag = 1;
+  let currentAngle = 0;
+  const updateTransform=()=> {
+     $logo.style.transform = `scale(${currentMag}) rotate(${angle}deg)`;
+  }
+  
+  const increaseMag=()=> {
+    if (currentMag <= 1.4) {
+      currentMag = currentMag + 0.2;
+      updateTransform();
+    } else {
+      alert('No more!');
+    }
+  }
+  
+  const decreaseMag=()=> {
+    if (currentMag >= 0.6) {
+      currentMag = currentMag - 0.2;
+      updateTransform();
+    } else {
+      alert('Please stop!');
+    }
+  }
+
   const clickHandler = (e) => {
     const $targetButton = e.currentTarget;
     let currentCount = parseInt($counter.textContent);
     if ($targetButton.textContent === 'Plus') {
       $counter.textContent = currentCount + 1;
-    } else {
+    } else if ($targetButton.textContent === 'Subtract') {
       $counter.textContent = currentCount - 1;
+    } else if ($targetButton.textContent === 'Magnification') {
+      increaseMag();
+    } else if ($targetButton.textContent === 'Reduction') {
+      decreaseMag();
+    } else {
+      currentAngle += 360;
+      updateTransform();
     }
   }
 
