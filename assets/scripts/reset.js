8a25a2c8aad906bac754b4d22bd610a2c24d80a2
GEEK <crew.sento0864@gmail.com>
2025-03-07 03:58:10 +0000

    js更新

--- assets/scripts/reset.js
+++ assets/scripts/reset.js
@@ -1,10 +1,18 @@
 (() => {
   // DOM
   const $counter = document.getElementById('js-counter');
+  const $logo = document.getElementById('js-logo');
+
+  function updateTransform() {
+    $logo.style.transform = `scale(${currentMag}) rotate(${currentAngle}deg)`;
+  }
 
   // click時の実行関数
   const clickHandler = () => {
     $counter.textContent = 0;
+    let currentMag = 1;
+    let currentAngle = 0;
+    updateTransform();
   }
 
   // click時に実行
