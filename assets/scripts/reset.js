ae27e54246bb73978ea0eaf9298a127c321cfef3
GEEK <crew.sento0864@gmail.com>
2025-03-07 17:21:07 +0900

    reset.js を更新

--- assets/scripts/reset.js
+++ assets/scripts/reset.js
@@ -1,18 +1,10 @@
 (() => {
   // DOM
   const $counter = document.getElementById('js-counter');
-  const $logo = document.getElementById('js-logo');
-
-  let currentMag = 1;
-  let currentAngle = 0;
-  function updateTransform() {
-    $logo.style.transform = `scale(${currentMag}) rotate(${currentAngle}deg)`;
-  }
 
   // click時の実行関数
   const clickHandler = () => {
     $counter.textContent = 0;
-    updateTransform();
   }
 
   // click時に実行
